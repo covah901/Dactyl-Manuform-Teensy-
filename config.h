e5a1395cfb327ab37715a93fa76ad8a27de7494e
covah901 <33563234+covah901@users.noreply.github.com>
2020-11-25 12:13:59 -0500

    Update config.h

--- config.h
+++ config.h
@@ -25,12 +25,12 @@
 #endif
 
 #ifdef MASTER_RIGHT
-#define MATRIX_ROW_PINS { B3, B1, F6, F7, F5 }
-#define MATRIX_COL_PINS { B2, B6, B5, B4, E6, D7 }
+#define MATRIX_ROW_PINS { F4, F5, F7, B1, B3 }
+#define MATRIX_COL_PINS { D3, D2, D4, C6, D7, E6 }
 #endif
 
-#define MATRIX_ROW_PINS_RIGHT { B3, B1, F6, F7, F5 }
-#define MATRIX_COL_PINS_RIGHT { B2, B6, B5, B4, E6, D7 }
+#define MATRIX_ROW_PINS_RIGHT { F4, F5, F7, B1, B3 }
+#define MATRIX_COL_PINS_RIGHT { D3, D2, D4, C6, D7, E6 }
 #define UNUSED_PINS
 #define USE_I2C
 // #define SOFT_SERIAL_PIN D1
@@ -48,8 +48,8 @@
 #define ENCODERS_PAD_B { D2 }
 #endif
 
-#define ENCODERS_PAD_A_RIGHT { D3 }
-#define ENCODERS_PAD_B_RIGHT { D2 }
+#define ENCODERS_PAD_A_RIGHT { B2 }
+#define ENCODERS_PAD_B_RIGHT { B6 }
 #define ENCODER_RESOLUTION 4
 
 #define RGBLIGHT_ANIMATIONS
